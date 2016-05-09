05dbcb6ef8111d5d2904f946858ffaa621f4af12
Pranay Joshi <pranay.joshi@smartstream-stp.com>
2016-05-09 15:31:28 +0530

    A non recursive code

--- src/pranayjoshi/SemiFinal1.java
+++ src/pranayjoshi/SemiFinal1.java
@@ -3,11 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pranayjoshi; 
+package pranayjoshi;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Random;
 
 /**
  *
@@ -15,57 +14,108 @@ import java.util.List;
  */
 public class SemiFinal1 {
 
-    /**
+	/**
      * @param args the command line arguments
      */
     //static List<Integer> op = new ArrayList<>();
     static int[] op ;
     static int k = 0;
     static int[] get(int[] input1,int input2){
-        op = new int [input2];
-        get1(input1,input2);
-        Arrays.sort(op);
-        
-        return Arrays.copyOfRange(op, 1, op.length);
+        op = new int [input2-1];
+        Arrays.sort(input1);
+        int[] output = get1(input1,input2);
+        while(output.length > 1){
+            output = get1(output,output.length);
+        }
+     //   return Arrays.copyOfRange(op, 0, op.length-1);
+     return op;
         
     }
     
-//    static int[] output;
-//    
     static int[] get1(int[] input1 ,int input2){
          if(input2 == 1){
             return new int[] {0};
         }
         
-        int[] output = new int[input2-1];
+       
         
-        Arrays.sort(input1);
-        output[0] = input1[0] + input1[1];
-        op[k++] = output[0];
-        int j=2;
-        for(int i=0;i<output.length -1;i++){
-            output[i+1] =  input1[j++];
-        }
-        get1(output,output.length);
+      int sum    = input1[0] + input1[1];
+        op[k++] = sum;
+        int[] output = getMergedArray(sum,input1,2);
+      //  get1(output,output.length);
         
         return output;
         
        // return output;
     }
     
+    static int[] getMergedArray(int elementToMerged ,int[] array,int startIndex){
+        int[] mergedSortedArry = new int[array.length - startIndex + 1];
+        int j=0;
+        boolean inserted = false;
+        for(int i=startIndex;i<array.length;i++){
+            if(elementToMerged <= array[i] && !inserted){
+                mergedSortedArry[j++] = elementToMerged;
+                mergedSortedArry[j++] =array[i];
+                inserted = true;
+            }else{
+                mergedSortedArry[j++] =array[i];
+            }
+        }
+        if(!inserted){ // element to be inserted in last location
+            mergedSortedArry[j] = elementToMerged;
+        }
+        return mergedSortedArry;
+    }
+    
+    
+  static void print(int[] array){
+      for(int i=0;i<array.length;i++){
+          System.out.println(array[i]);
+      }
+  }
+    
     
     public static void main(String[] args) {
         // TODO code application logic here
-        //int[] input1 = {4,2,3,6};
-        int[] input1 = {1,2,3,4,5,6};
-        int[] output = get(input1,6);
+      //  int[] input1 = {4,2,3,6};
+       // int[] input1 = {1,2,3,4,5,6};
+        //int[] input1 = {1,1,1,1,1,1,1,1};
+        int[] input1 = getRandomInputArray(80000);
+    //    print(input1);
+        long startTime = System.currentTimeMillis();
+        int[] output = get(input1,input1.length);
+        long endTime = System.currentTimeMillis();
         int sum = 0;
         for(int i=0;i<output.length;i++){
-            System.out.println(output[i]);
+        //    System.out.println(output[i]);
             sum = sum + output[i];
         }
         
         System.out.println("Sum " +sum);
+        System.out.println(" Time taken " +(endTime - startTime));
+
+
+//int[] input1 = {1,1,1,1,1,1};
+//int[] output = getMergedArray(2, input1, 2);
+//        print(output);
+
+
+    }
+    
+    static int[] getRandomInputArray(int length){
+        int[] output = new int[length];
+        for(int i=0;i<length;i++){
+            int nextValue = getRandomNumber(10000);
+            output[i] = nextValue == 0 ? 1 : nextValue;
+        }
+        
+        return output;
+    }
+     static Random rn = new Random();
+    static int getRandomNumber(int maximum){
+       
+return rn.nextInt(maximum);
     }
     
 }
