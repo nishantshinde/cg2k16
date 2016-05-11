2777c019aebbbe2b7b78c1e561cfe8282fd8a0b4
Nishant Shinde <nishantshinde@gmail.com>
2016-05-11 17:43:03 +0100

    Added test cases for border cases

--- test/SemiFinalProblem2Test.java
+++ test/SemiFinalProblem2Test.java
@@ -7,16 +7,24 @@ public class SemiFinalProblem2Test {
 			@Override
 			public void doWork(int input1, int input2, int input3, String input4) {
 				System.out.println(
-						//SemiFinalProblem2.maxTreeDestroyed(input1, input2, input3, input4)
-						SemiFinal2Nish.maxTreeDestroyed(input1, input2, input3, input4)
+						"SemiFinalProblem2 -> " + SemiFinalProblem2.maxTreeDestroyed(input1, input2, input3, input4)
+				);
+				System.out.println(
+						"SemiFinal2Nish -> " + SemiFinal2Nish.maxTreeDestroyed(input1, input2, input3, input4)
 				);
 			}
     	};
 
-    	testCase1(worker);
-   // 	testCase2(worker);
-    //	testCase3(worker);
+//    	System.out.println("Test case 1 results (below) ");testCase1(worker); System.out.println();
+//    	System.out.println("Test case 2 results (below) "); testCase2(worker); System.out.println();
+//    	System.out.println("Test case 3 results (below) "); testCase3(worker); System.out.println();
+//    	System.out.println("Test case 4 results (expected 3) "); testCase4(worker); System.out.println();
+//    	System.out.println("Test case 5 results (expected 5) "); testCase5(worker); System.out.println();
+    	System.out.println("Test case 6 results (expected 5) "); testCase6(worker); System.out.println();
+    	System.out.println("Test case 7 results (expected 3) "); testCase7(worker); System.out.println();
+    	System.out.println("Test case 8 results (expected 3) "); testCase8(worker); System.out.println();
 
+    	
     }
     
     static void testCase1(Worker worker) {
@@ -34,6 +42,31 @@ public class SemiFinalProblem2Test {
     static void testCase3(Worker worker) {
     	String destroyed_positions = "((2,1),(2,12),(2,23))";
     	worker.doWork(2, 23, 3, destroyed_positions);
+    }
+    
+    static void testCase4(Worker worker) {
+    	String destroyed_positions = "((3,1),(3,3),(3,5),(5,2),(5,3),(5,4),(5,5))";
+    	worker.doWork(5, 5, 7, destroyed_positions);
+    }
+    
+    static void testCase5(Worker worker) {
+    	String destroyed_positions = "((1,1),(1,2),(1,3),(1,4),(1,5),(3,1),(3,3),(3,5))";
+    	worker.doWork(5, 5, 8, destroyed_positions);
+    }
+
+    static void testCase6(Worker worker) {
+    	String destroyed_positions = "((1,1),(2,1),(3,1),(3,3),(3,5),(4,1),(5,1))";
+    	worker.doWork(5, 5, 7, destroyed_positions);
+    }
+
+    static void testCase7(Worker worker) {
+    	String destroyed_positions = "((1,5),(2,5),(3,1),(3,3),(3,5),(4,5))";
+    	worker.doWork(5, 5, 6, destroyed_positions);
+    }
+
+    static void testCase8(Worker worker) {
+    	String destroyed_positions = "((2,5),(3,1),(3,3),(3,5),(4,5),(5,5))";
+    	worker.doWork(5, 5, 6, destroyed_positions);
     }
     
     
